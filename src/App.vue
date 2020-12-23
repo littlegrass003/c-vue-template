4c768a26486799191127dffceb770cd301e9c545
cwz <510754059@qq.com>
2020-12-23 16:06:51 +0800

    change info

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,11 @@
     <router-view></router-view>
     <div>
       <router-link to="/home">首页</router-link>
+      ｜
       <router-link to="/login">登录</router-link>
     </div>
     <img alt="Vue logo" src="./assets/img/logo.png">
-    <HelloWorld msg="a coderwhy vue template"/>
+    <HelloWorld msg="Auth:Cwz."/>
   </div>
 </template>
 
